7a11f9e25ffc1d75712ffdab7a66ff99937eaf98
matheusmgp <matheus_mgp@hotmail.com>
2021-09-09 19:59:16 -0300

    Exemplo de producer e consumer com rabbitMQ usando C#

---
+++ RConsumer/RConsumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+               /* channel.QueueDeclare(queue: "minhaNovaFila",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);*/
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "minhaNovaFila",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                channel.BasicConsume(queue: "minhaSegundaFila",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
