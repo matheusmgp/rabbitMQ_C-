3038c42f7de7bb3ea3ee0a1a004907136c1d1693
matheusmgp <matheus_mgp@hotmail.com>
2021-09-09 22:03:14 -0300

    mandando para duas filas diferentes

--- EProducer/EProducer/Program.cs
+++ EProducer/EProducer/Program.cs
@@ -12,28 +12,46 @@ namespace EProducer
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "minhaSegundaFila",
+                /*channel.QueueDeclare(queue: "minhaSegundaFila",
                                      durable: true,
                                      exclusive: false,
                                      autoDelete: false,
-                                     arguments: null);
+                                     arguments: null);*/
 
-                int count = 0;
+                int count1 = 0;
+                int count2 = 0;
 
                 while (true)
                 {
-                    string message = $"{count ++} Hello World!";
+                    string message = $"{count1++} Hello World! na minhaNovaFila";
                     var body = Encoding.UTF8.GetBytes(message);
 
                     channel.BasicPublish(exchange: "minhaNovaExchange",
-                                         routingKey: "minhaSegundaKey",
+                                         routingKey: "minhaRoutingKey", 
                                          basicProperties: null,
                                          body: body);
                     Console.WriteLine(" [x] Sent {0}", message);
                     System.Threading.Thread.Sleep(200);
 
+
+                    string message2 = $"{count2++} Hello World! na minhaSegundaFila";
+                    var body2 = Encoding.UTF8.GetBytes(message2);
+
+                    channel.BasicPublish(exchange: "minhaNovaExchange",
+                                            routingKey: "minhaSegundaKey",
+                                            basicProperties: null,
+                                            body: body2);
+                    Console.WriteLine(" [x] Sent {0}", message2);
+
+                    System.Threading.Thread.Sleep(500);
+
+
+
+
                 }
-             
+
+               
+
             }
 
            
